4bb9bfb1f879cec3f13fbe987320dcfe0efd85d4
danfra2594 <daniel.franco@proalnet.com>
2020-04-18 16:51:43 -0500

    Se crea proyecto base de arquitectura

---
+++ RestAPI_POI/RestAPI_POI/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace RestAPI_POI.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ RestAPI_POI/RestAPI_POI/Areas/HelpPage/ModelDescriptions/IModelDocumentationProvider.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Reflection;
+
+namespace RestAPI_POI.Areas.HelpPage.ModelDescriptions
+{
+    public interface IModelDocumentationProvider
+    {
+        string GetDocumentation(MemberInfo member);
+
+        string GetDocumentation(Type type);
+    }
+}
